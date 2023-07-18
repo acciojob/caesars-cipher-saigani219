a0139694bf232dd6d966b9b8f1a5a14ba571ac95
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-18 18:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,8 +34,14 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+  for(let i = 0; i < encodedStr.length; i++){
+	  let char = encodedStr.charAt(i);
+	  if(char >= 65 && char <= 90)
+		  decodedArr += lookup[char];
+	  else
+		  decodedArr += char;
+  }
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
