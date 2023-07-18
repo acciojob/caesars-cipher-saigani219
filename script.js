39fa0fc8ea36ff00d05be98b14b3534aca17fa19
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-18 18:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  let decodedArr = ""; // Your Result goes here
   // Only change code below this line
   for(let i = 0; i < encodedStr.length; i++){
 	  let char = encodedStr.charAt(i);
